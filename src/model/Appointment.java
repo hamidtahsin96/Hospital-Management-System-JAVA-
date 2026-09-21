3d478e4c6f7779471dc8763d552f5b8a0d30def6
Hamid Tahsin <hamidtahsin96@gmail.com>
2026-09-21 22:21:59 +0600

    Done Receptionist, DoctorPatient, Apoointment, MedicalRecord, Payment

---
+++ src/model/Appointment.java
@@ -0,0 +1,130 @@
+package model;
+import java.lang.*;
+
+public class Appointment
+{
+    private String appointmentId;
+    private Doctor doctor;
+    private Patient patient;
+    private String date;
+    private String time;
+    private String status;
+
+    public Appointment()
+    {
+
+    }
+
+    public Appointment(String appointmentId, Doctor doctor, Patient patient,
+                       String date, String time, String status)
+    {
+        this.appointmentId = appointmentId;
+        this.doctor = doctor;
+        this.patient = patient;
+        this.date = date;
+        this.time = time;
+        this.status = status;
+    }
+
+    public void setAppointmentId(String appointmentId)
+    {
+        this.appointmentId = appointmentId;
+    }
+
+    public void setDoctor(Doctor doctor)
+    {
+        this.doctor = doctor;
+    }
+
+    public void setPatient(Patient patient)
+    {
+        this.patient = patient;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public void setTime(String time)
+    {
+        this.time = time;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    public String getAppointmentId()
+    {
+        return this.appointmentId;
+    }
+
+    public Doctor getDoctor()
+    {
+        return this.doctor;
+    }
+
+    public Patient getPatient()
+    {
+        return this.patient;
+    }
+
+    public String getDate()
+    {
+        return this.date;
+    }
+
+    public String getTime()
+    {
+        return this.time;
+    }
+
+    public String getStatus()
+    {
+        return this.status;
+    }
+
+    public String toStringAppointment()
+    {
+        String str = this.appointmentId + "," +
+                this.doctor.getUserId() + "," +
+                this.patient.getUserId() + "," +
+                this.date + "," +
+                this.time + "," +
+                this.status + "\n";
+
+        return str;
+    }
+
+    public Appointment formAppointment(String str)
+    {
+        String information[] = str.split(",");
+
+        if(information.length >= 6)
+        {
+            Appointment ap = new Appointment();
+
+            ap.setAppointmentId(information[0]);
+
+            Doctor doctor = new Doctor();
+            doctor.setUserId(information[1]);
+            ap.setDoctor(doctor);
+
+            Patient patient = new Patient();
+            patient.setUserId(information[2]);
+            ap.setPatient(patient);
+
+            ap.setDate(information[3]);
+            ap.setTime(information[4]);
+            ap.setStatus(information[5]);
+
+            return ap;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ src/model/MedicalRecord.java
@@ -0,0 +1,101 @@
+package model;
+import java.lang.*;
+
+public class MedicalRecord
+{
+    private String medicalRecordId;
+    private Doctor doctor;
+    private Patient patient;
+    private String details;
+
+    public MedicalRecord()
+    {
+
+    }
+
+    public MedicalRecord(String medicalRecordId, Doctor doctor, Patient patient, String details)
+    {
+        this.medicalRecordId = medicalRecordId;
+        this.doctor = doctor;
+        this.patient = patient;
+        this.details = details;
+    }
+
+    public void setMedicalRecordId(String medicalRecordId)
+    {
+        this.medicalRecordId = medicalRecordId;
+    }
+
+    public void setDoctor(Doctor doctor)
+    {
+        this.doctor = doctor;
+    }
+
+    public void setPatient(Patient patient)
+    {
+        this.patient = patient;
+    }
+
+    public void setDetails(String details)
+    {
+        this.details = details;
+    }
+
+    public String getMedicalRecordId()
+    {
+        return this.medicalRecordId;
+    }
+
+    public Doctor getDoctor()
+    {
+        return this.doctor;
+    }
+
+    public Patient getPatient()
+    {
+        return this.patient;
+    }
+
+    public String getDetails()
+    {
+        return this.details;
+    }
+
+    public String toStringMedicalRecord()
+    {
+        String str = this.medicalRecordId + "," +
+                this.doctor.getUserId() + "," +
+                this.patient.getUserId() + "," +
+                this.details + "\n";
+
+        return str;
+    }
+
+    public MedicalRecord formMedicalRecord(String str)
+    {
+        String information[] = str.split(",");
+
+        if(information.length >= 4)
+        {
+            MedicalRecord mr = new MedicalRecord();
+
+            mr.setMedicalRecordId(information[0]);
+
+            Doctor doctor = new Doctor();
+            doctor.setUserId(information[1]);
+            mr.setDoctor(doctor);
+
+            Patient patient = new Patient();
+            patient.setUserId(information[2]);
+            mr.setPatient(patient);
+
+            mr.setDetails(information[3]);
+
+            return mr;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
